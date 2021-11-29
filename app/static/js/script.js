c64739ee388db1f539eb7bbff9c007ad445cd35c
Chey Rose <cheyroseflammer@gmail.com>
2021-11-28 20:57:25 -0800

    top 50 bar chart

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,19 +1,58 @@
-// SAMPLE FUNCTION SHOWING HOW TO GRAB JSON DATA
-function init() {
-  // Populate select options
-  d3.json("/static/data/games.json").then((results) => {
-    let gameList = []
-    let data = results.data
-    for (let i = 0; i < data.length; i++){
-      gameList.push(data[i])
-      let gameName = gameList[i]["name"]
-      let gameRank = gameList[i]["rank"]
-      let gameSales = gameList[i]["global_sales"]
-    }
-  });
-}
-init()
-
+// Read in data with JSON
+d3.json('static/data/games.json').then(function (json_data){
+  // Grab json data
+  let data = json_data
+  // Grab specific variables
+  let sampleData = []
+  let sampleRank = []
+  let sampleNames = []
+  let sampleSales = []
+  let samplePublishers = []
+  let sampleGenre = []
+  let samplePlatform = []
+  // Loop through 100 
+  for (let i = 0; i <= 49; i++){
+    sampleData.push(data[i])
+    sampleRank.push(data[i].rank)
+    sampleNames.push(data[i].name)
+    sampleSales.push(data[i].global_sales)
+    samplePublishers.push(data[i].publisher)
+    sampleGenre.push(data[i].genre)
+    samplePlatform.push(data[i].platform)
+  }
+  let xticks = sampleRank.map(x => + x).slice(0,100).reverse();
+  let barData = [{
+    x: xticks,
+    y: sampleSales,
+    text: sampleNames,
+    // base: [sampleSales],
+    type: 'bar',
+    hoverinfo: sampleNames.slice(0,100).reverse(),
+    marker: {
+      color: "maroon",
+    },
+  }]
+  let barLayout = {
+    width: 800,
+    height: 400,
+    title: "Global Sales Data",
+    xaxis: {title: "Game Rank"},
+    yaxis: {title: "Global Sales $"},
+    hovermode: sampleNames,
+    plot_bgcolor: "black",
+    paper_bgcolor: "#0d0d0d",
+    font: {
+      color: "white",
+      family: 'Roboto, san-serif',
+    },
+    margin: {
+      pad: 4
+    },
+    automargin: true,
+    autosize: false,
+  }
+  Plotly.newPlot("bar", barData, barLayout)
+})
 
 
 
