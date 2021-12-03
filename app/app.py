0e502dd431f6cba69ee3b186e65cfa592f4c566c
marcuspttr <marcuspttr@gmail.com>
2021-12-02 21:57:58 -0800

    Carousel is fully loaded. Almost done.

--- app/app.py
+++ app/app.py
@@ -8,14 +8,42 @@ from bs4 import BeautifulSoup as bs
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
+def scrape():
+    # Splinter set up
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    # Create dictonary instance
+    dict = {}
+    # URL
+    url = "https://www.wepc.com/news/video-game-statistics/"
+    # Request response from URL
+    browser.visit(url)
+    # Assign the document grab to a variable
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")
+    quotes = []
+    
+    # Pulls all of the website's game quotes.
+    blockquote = soup.find_all('p', class_= "elementor-blockquote__content")
+    
+    # Grabs the text of the first 12 and saves them into a list.
+    i = 0
+    while i <= 11:
+        quotes.append(blockquote[i].get_text())
+        i+=1
+    return quotes
+
+
 # Init App
 app = Flask(__name__)
 
 # Route Section
 @app.route('/')
 def index():
-    with open('templates/index.html') as f:
-        return f.read()
+    quotes = scrape()
+    return render_template("index.html", quotes = quotes)
 
 @app.route("/static/js/<filename>")
 def get_static(filename):
@@ -60,34 +88,6 @@ def datafinder():
         na_sales, eu_sales, jp_sales, other_sales, global_sales in data]
 
     return jsonify(data)
-
-@app.route('/scrape')
-def scrape():
-    # Splinter set up
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
-    # Create dictonary instance
-    dict = {}
-    # URL
-    url = "https://www.wepc.com/news/video-game-statistics/"
-    # Request response from URL
-    browser.visit(url)
-    # Assign the document grab to a variable
-
-    # Scrape page into Soup
-    html = browser.html
-    soup = bs(html, "html.parser")
-    quotes = []
-    
-    # Pulls all of the website's game quotes.
-    blockquote = soup.find_all('p', class_= "elementor-blockquote__content")
-    
-    # Grabs the text of the first 12 and saves them into a list.
-    i = 0
-    while i <= 11:
-        quotes.append(blockquote[i].get_text())
-        i+=1
-    return quotes
 
 if __name__ == "__main__":
     app.run(debug=True)
